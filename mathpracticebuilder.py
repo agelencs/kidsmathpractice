16681e08d68087b06819122adec0f8a248b58788
Gelencser <agelencser5@gmail.com>
2020-03-29 14:35:50 +0100

    refactored and changed function names

--- mathpracticebuilder.py
+++ mathpracticebuilder.py
@@ -8,20 +8,22 @@ def getrandomnumber():
     value = randint(0, 12)
     return value
 
-def getmultiplication():
+
+def createmultiplication():
     val1 = getrandomnumber()
     val2 = getrandomnumber()
     textbuilt = str(val1) + " x " + str(val2) + " ="
     return textbuilt
 
-def getdivision():
-    val1=getrandomnumber()
+
+def createdivision():
+    val1 = getrandomnumber()
     val2 = getrandomnumber()
-    while val1==0 or val2==0:
+    while val1 == 0 or val2 == 0:
         val1 = getrandomnumber()
         val2 = getrandomnumber()
-    res=val1*val2
-    textbuilt=str(res)+" ÷ "+str(val2)+" ="
+    res = val1 * val2
+    textbuilt = str(res) + " ÷ " + str(val2) + " ="
     return textbuilt
 
 
@@ -38,19 +40,19 @@ for section in sections:
     section.top_margin = Cm(1)
 
 for i in range(99):
-    t=randint(0,1)
-    if t==0:
-        task=getmultiplication()
+    t = randint(0, 1)
+    if t == 0:
+        task = createmultiplication()
     else:
-        task=getdivision()
-    if i<33:
+        task = createdivision()
+    if i < 33:
         cell = table.cell(i, 0)
         cell.text = task
-    if i>33 and i<=66:
-        cell = table.cell(i-34, 1)
+    if i > 33 and i <= 66:
+        cell = table.cell(i - 34, 1)
         cell.text = task
     if i > 66:
-        cell = table.cell(i-67, 2)
+        cell = table.cell(i - 67, 2)
         cell.text = task
 
 docu.save('test.doc')
